e0776523105c471aec0c3c2bfebc0deccccee237
SolasTim <73838560+SolasTim@users.noreply.github.com>
2022-02-24 12:42:03 +0000

    added boundaries for player

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ playerX = 380
 playerY = 260
 playerX_change = 0
 
+
 class character:
     def __init__(self, x, y, img, vel):
         self.x = x
@@ -30,17 +31,34 @@ class character:
             if event.key == pygame.K_LEFT:
                 print("Left")
                 self.x -= self.vel
-                return self.x
+                if self.x <= 0:
+                    self.x = 0
+                    return self.x
+                else:
+                    return self.x
             if event.key == pygame.K_RIGHT:
                 print("Right")
                 self.x += self.vel
-                return self.x
+                if self.x >= 754:
+                    self.x = 754
+                else:
+                    return self.x
             if event.key == pygame.K_UP:
                 print("up")
                 self.y -= self.vel
+                if self.y <= 0:
+                    self.y = 0
+                    return self.y
+                else:
+                    return self.y
             if event.key == pygame.K_DOWN:
                 print("down")
                 self.y += self.vel
+                if self.y >= 530:
+                    self.y = 530
+                    return self.y
+                else:
+                    return self.y
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 print("Key released")
@@ -54,14 +72,12 @@ cowboy = character(playerX, playerY, playerImg, 1)
 # game loop
 running = True
 while running:
-    
-#    playerX += 0.1
+
+    #    playerX += 0.1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-
 
     cowboy.KeyStroke()
     screen.fill((255, 255, 255))
