2a5a17239a38cca062adc044a81948ebe5e7d7de
SolasTim <73838560+SolasTim@users.noreply.github.com>
2022-03-29 09:53:14 +0100

    cant import my classes from other files
    leaving as one file for now

--- menu.py
+++ menu.py
@@ -1,4 +1,9 @@
 import pygame
+from menu import *
+
+#create display window
+SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 800
 
 class Button:
     def __init__(self, x, y, image, scale):
@@ -28,3 +33,34 @@ class Button:
 
         return action
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Button Demo')
+
+#load button images
+start_img = pygame.image.load('start_btn.png').convert_alpha()
+exit_img = pygame.image.load('exit_btn.png').convert_alpha()
+
+#create button instances
+start_button = Button(100, 200, start_img, 0.8)
+exit_button = Button(450, 200, exit_img, 0.8)
+
+#game loop
+run = True
+while run:
+
+	screen.fill((200, 200, 200))
+
+	if start_button.draw(screen):
+		print('START')
+	if exit_button.draw(screen):
+		print('EXIT')
+
+	#event handler
+	for event in pygame.event.get():
+		#quit game
+		if event.type == pygame.QUIT:
+			run = False
+
+	pygame.display.update()
+
+pygame.quit()
